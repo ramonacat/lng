72755c2cef9fe1b21f8f0606f9a13aa31d12f4d5
Ramona Łuczkiewicz <ja@agares.info>
2025-04-13 11:38:51 +0200

    refactoring

--- src/type_check.rs
+++ src/type_check.rs
@@ -30,36 +30,45 @@ pub fn type_check(program: &Program) -> Result<(), TypeCheckError> {
         };
         for statement in body_statements.iter() {
             match statement {
-                Statement::Expression(expression) => match expression {
-                    Expression::FunctionCall { name, arguments } => {
-                        let Some(called_function) = declared_functions.get(name.as_str()) else {
-                            return Err(TypeCheckError::CallingUndeclaredFunction(
-                                name.to_string(),
-                            ));
-                        };
-
-                        if arguments.len() != called_function.arguments.len() {
-                            return Err(TypeCheckError::IncorrectNumberOfArgumentsPassed(
-                                called_function.name.clone(),
-                            ));
-                        }
-
-                        for (idx, argument) in arguments.iter().enumerate() {
-                            let type_ = determine_expression_type(&declared_functions, argument)?;
-                            let expected_type = &called_function.arguments.get(idx).unwrap().type_;
-
-                            if &type_ != expected_type {
-                                return Err(TypeCheckError::UnexpectedArgumentTypeInFunctionCall(
-                                    name.to_string(),
-                                    idx,
-                                ));
-                            }
-                        }
-                    }
-                    Expression::Literal(_) => {}
-                },
+                Statement::Expression(expression) => {
+                    type_check_expression(expression, &declared_functions)?
+                }
             }
         }
+    }
+
+    Ok(())
+}
+
+fn type_check_expression(
+    expression: &Expression,
+    declared_functions: &HashMap<String, Function>,
+) -> Result<(), TypeCheckError> {
+    match expression {
+        Expression::FunctionCall { name, arguments } => {
+            let Some(called_function) = declared_functions.get(name.as_str()) else {
+                return Err(TypeCheckError::CallingUndeclaredFunction(name.to_string()));
+            };
+
+            if arguments.len() != called_function.arguments.len() {
+                return Err(TypeCheckError::IncorrectNumberOfArgumentsPassed(
+                    called_function.name.clone(),
+                ));
+            }
+
+            for (idx, argument) in arguments.iter().enumerate() {
+                let type_ = determine_expression_type(declared_functions, argument)?;
+                let expected_type = &called_function.arguments.get(idx).unwrap().type_;
+
+                if &type_ != expected_type {
+                    return Err(TypeCheckError::UnexpectedArgumentTypeInFunctionCall(
+                        name.to_string(),
+                        idx,
+                    ));
+                }
+            }
+        }
+        Expression::Literal(_) => {}
     }
 
     Ok(())
@@ -70,9 +79,12 @@ fn determine_expression_type(
     expression: &Expression,
 ) -> Result<Type, TypeCheckError> {
     match expression {
-        Expression::FunctionCall { name, .. } => {
-            Ok(declared_functions.get(name).map(|x| Ok(x)).unwrap_or_else(|| Err(TypeCheckError::CallingUndeclaredFunction(name.clone())))?.return_type.clone())
-        }
+        Expression::FunctionCall { name, .. } => Ok(declared_functions
+            .get(name)
+            .map(|x| Ok(x))
+            .unwrap_or_else(|| Err(TypeCheckError::CallingUndeclaredFunction(name.clone())))?
+            .return_type
+            .clone()),
         Expression::Literal(literal) => match literal {
             crate::ast::Literal::String(_) => Ok(Type::Named("string".to_string())),
         },
